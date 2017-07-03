64d3f51fb321cf234e51d73dec52f62eac26032a
wataridori2010 <wataridori2010@gmail.com>
2017-07-03 23:12:38 +0900

    update

--- videostab_gyro.cpp
+++ videostab_gyro.cpp
@@ -36,9 +36,26 @@ int main(int argc, char **argv)
     vector<double> gyrX;
     vector<double> gyrY;
     vector<double> gyrZ;
+    char path[512]="/Users/itoyuichi/github/playground/OpenCV/videostab/EIS/";
+    char path_[512]="";
+    //char fn[512]="VID_20170609_231526";
+    //char fn[512]="VID_20170701_182046";// translation
+    //char fn[512]="VID_20170701_182111"; // hand-held
+    //char fn[512]="VID_20170701_100752"; //
+    char fn[512]="VID_20170701_100826";
+    //char fn[512]="VID_20170609_231526";
+    
+    char gyro[512]="gyro.csv";
+    char video[512]=".mp4";
+    char fndst[512]="_result.mp4";
+    
     
     double time=0,gx = 0,gy=0,gz=0;
-    FILE *fp=fopen("/Users/itoyuichi/github/playground/OpenCV/stab-opencv/VID_20170609_230954gyro.csv","r");
+//    FILE *fp=fopen("/Users/itoyuichi/github/playground/OpenCV/stab-opencv/VID_20170609_230954gyro.csv","r");
+//    FILE *fp=fopen("/Users/itoyuichi/github/playground/OpenCV/stab-opencv/VID_20170609_231526gyro.csv","r");
+    strcpy(path_,path);
+    FILE *fp=fopen(strcat(strcat(path_,fn),gyro),"r");
+    printf("%s\n",path_);
     char buf[256];
     fscanf(fp,"%s\n",buf );
     printf("%s\n",buf);
@@ -59,7 +76,7 @@ int main(int argc, char **argv)
     // copy and initialize
     for(int i=0; i< timeStamp.size(); i++) timeStamp2[i]=timeStamp[i]-timeStamp[0];
 
-
+/*
     // remove low freqency
     double *gyrX_low=(double *)calloc(timeStamp.size(),sizeof(double));
     double *gyrY_low=(double *)calloc(timeStamp.size(),sizeof(double));
@@ -67,7 +84,7 @@ int main(int argc, char **argv)
     double gyrX_rec=gyrX[0];
     double gyrY_rec=gyrY[0];
     double gyrZ_rec=gyrZ[0];
-    double rec=0.99;
+    double rec=0.98;
 
     for(int i=0; i< timeStamp.size()-1; i++){
         gyrX_rec = rec * gyrX_rec + (1-rec) * gyrX[i];
@@ -82,7 +99,7 @@ int main(int argc, char **argv)
         gyrY[i] = gyrY[i] - gyrY_low[i];
         gyrZ[i] = gyrZ[i] - gyrZ_low[i];
     }
-
+*/
 /*
     int sw=60;
     for(int i=0; i< timeStamp.size()-1; i++){
@@ -111,10 +128,14 @@ int main(int argc, char **argv)
     }
 */
     
-    free(gyrX_low);
-    free(gyrY_low);
-    free(gyrZ_low);
+//    free(gyrX_low);
+//    free(gyrY_low);
+//    free(gyrZ_low);
 
+    
+//    double *rotX_sm=(double *)calloc(timeStamp.size(),sizeof(double));
+//    double *rotY_sm=(double *)calloc(timeStamp.size(),sizeof(double));
+//    double *rotZ_sm=(double *)calloc(timeStamp.size(),sizeof(double));
     
     // accumulating
     double denom=1000000000;
@@ -125,12 +146,12 @@ int main(int argc, char **argv)
 //        rotX[i]=rotX[i-1]+((timeStamp2[i]-timeStamp2[i-1])*(gyrX[i-1]+gyrX[i])*0.5)/denom;
 //        rotY[i]=rotY[i-1]+((timeStamp2[i]-timeStamp2[i-1])*(gyrY[i-1]+gyrY[i])*0.5)/denom;
 //        rotZ[i]=rotZ[i-1]+((timeStamp2[i]-timeStamp2[i-1])*(gyrZ[i-1]+gyrZ[i])*0.5)/denom;
-//        rotX[i]=rotX[i-1]+((timeStamp2[i]-timeStamp2[i-1])*(gyrX[i-1]+gyrX[i]+gyrX[i+1])*0.333)/denom;
-//        rotY[i]=rotY[i-1]+((timeStamp2[i]-timeStamp2[i-1])*(gyrY[i-1]+gyrY[i]+gyrY[i+1])*0.333)/denom;
-//        rotZ[i]=rotZ[i-1]+((timeStamp2[i]-timeStamp2[i-1])*(gyrZ[i-1]+gyrZ[i]+gyrZ[i+1])*0.333)/denom;
-        rotX[i]=rotX[i-1]+((timeStamp2[i]-timeStamp2[i-1])*(gyrX[i-1]+2*gyrX[i]+gyrX[i+1])*0.25)/denom;
-        rotY[i]=rotY[i-1]+((timeStamp2[i]-timeStamp2[i-1])*(gyrY[i-1]+2*gyrY[i]+gyrY[i+1])*0.25)/denom;
-        rotZ[i]=rotZ[i-1]+((timeStamp2[i]-timeStamp2[i-1])*(gyrZ[i-1]+2*gyrZ[i]+gyrZ[i+1])*0.25)/denom;
+        rotX[i]=rotX[i-1]+((timeStamp2[i]-timeStamp2[i-1])*(gyrX[i-1]+gyrX[i]+gyrX[i+1])*0.333)/denom;
+        rotY[i]=rotY[i-1]+((timeStamp2[i]-timeStamp2[i-1])*(gyrY[i-1]+gyrY[i]+gyrY[i+1])*0.333)/denom;
+        rotZ[i]=rotZ[i-1]+((timeStamp2[i]-timeStamp2[i-1])*(gyrZ[i-1]+gyrZ[i]+gyrZ[i+1])*0.333)/denom;
+//        rotX[i]=rotX[i-1]+((timeStamp2[i]-timeStamp2[i-1])*(gyrX[i-1]+2*gyrX[i]+gyrX[i+1])*0.25)/denom;
+//        rotY[i]=rotY[i-1]+((timeStamp2[i]-timeStamp2[i-1])*(gyrY[i-1]+2*gyrY[i]+gyrY[i+1])*0.25)/denom;
+//        rotZ[i]=rotZ[i-1]+((timeStamp2[i]-timeStamp2[i-1])*(gyrZ[i-1]+2*gyrZ[i]+gyrZ[i+1])*0.25)/denom;
         printf("timeStamp2: %lf, %lf\n",timeStamp2[i]-timeStamp2[i-1],timeStamp[i]-timeStamp[i-1]);
     }
     for(int i=1; i< timeStamp.size()-1; i++){
@@ -149,17 +170,29 @@ int main(int argc, char **argv)
     
     VideoWriter writer;//("test1.avi", CV_FOURCC_DEFAULT, 10, Size(640, 480), true);
     
-    VideoCapture cap("/Users/itoyuichi/github/playground/OpenCV/stab-opencv/VID_20170609_230954.mp4");
+//    VideoCapture cap("/Users/itoyuichi/github/playground/OpenCV/stab-opencv/VID_20170609_230954.mp4");
+//    VideoCapture cap("/Users/itoyuichi/github/playground/OpenCV/stab-opencv/VID_20170609_231526.mp4");
+//    VideoCapture cap("/Users/itoyuichi/github/playground/OpenCV/stab-opencv/VID_20170609_231125.mp4");
+    strcpy(path_,path);
+    VideoCapture cap(strcat(strcat(path_,fn),video));
+    printf("%s\n",path_);
+    
     Mat cur, cur2;
     Mat canvas;
 
     double fps = cap.get(CV_CAP_PROP_FPS);
     cout << "Frames per second using video.get(CV_CAP_PROP_FPS) : " << fps << endl;
     
-    int k=0, k_gyro=0, max_frames = 360;
+    int k=0, k_gyro=0, max_frames = 1000;
     
-    while(k < max_frames-1) { // don't process the very last frame, no valid transform
-//    while(1) {
+    Mat Tsm(3,3,CV_64F);
+    Tsm.at<double>(0,0) = 1; Tsm.at<double>(0,1) = 0; Tsm.at<double>(0,2) = 0;
+    Tsm.at<double>(1,0) = 0; Tsm.at<double>(1,1) = 1; Tsm.at<double>(1,2) = 0;
+    Tsm.at<double>(2,0) = 0; Tsm.at<double>(2,1) = 0; Tsm.at<double>(2,2) = 1;
+
+    
+//    while(k < max_frames-1) { // don't process the very last frame, no valid transform
+    while(1) {
         
         cap >> cur;
         
@@ -172,22 +205,31 @@ int main(int argc, char **argv)
         //float time=float(k)/24.9423;
         //while(time>timeStamp2[k_gyro])k_gyro++;
         while(st>timeStamp2[k_gyro])k_gyro++;
+        double time=st;
         
         //printf("st: %lf, time:%f\n",st,time);
         //printf("k_gyro: %d\n",k_gyro);
         
-//        if(k_gyro>0)k_gyro=k_gyro-1;
-        
-//        float rx=rotX[3*(k+5)]; // should adjust time stamp!!! and Delay
-//        float ry=rotY[3*(k+5)];
-//        float rz=rotZ[3*(k+5)];
-        float rx=rotX[k_gyro]; // should adjust time stamp!!! and Delay
-        float ry=rotY[k_gyro];
-        float rz=rotZ[k_gyro];
-//          float rx=((time-timeStamp2[k_gyro-1])*rotX[k_gyro]+(timeStamp2[k_gyro]-time)*rotX[k_gyro-1])/(timeStamp2[k_gyro]-timeStamp2[k_gyro-1]);
-//          float ry=((time-timeStamp2[k_gyro-1])*rotY[k_gyro]+(timeStamp2[k_gyro]-time)*rotX[k_gyro-1])/(timeStamp2[k_gyro]-timeStamp2[k_gyro-1]);
-//          float rz=((time-timeStamp2[k_gyro-1])*rotZ[k_gyro]+(timeStamp2[k_gyro]-time)*rotX[k_gyro-1])/(timeStamp2[k_gyro]-timeStamp2[k_gyro-1]);
+//        if(k_gyro>10)k_gyro=k_gyro-10;
 
+        int pa=5;//4;//4;//3
+        float rx=rotX[k_gyro+pa]; // should adjust time stamp!!! and Delay
+        float ry=rotY[k_gyro+pa];
+        float rz=rotZ[k_gyro+pa];
+//        double rate=(time-timeStamp2[k_gyro-1])/(timeStamp2[k_gyro]-timeStamp2[k_gyro-1]);
+//        float rx=rate*rotX[k_gyro+0]+(1-rate)*rotX[k_gyro-1];
+//        float ry=rate*rotY[k_gyro+0]+(1-rate)*rotY[k_gyro-1];
+//        float rz=rate*rotZ[k_gyro+0]+(1-rate)*rotZ[k_gyro-1];
+        
+        
+//        float rx=((st-timeStamp2[k_gyro-1])*rotX[k_gyro]+(timeStamp2[k_gyro]-st)*rotX[k_gyro-1])/(timeStamp2[k_gyro]-timeStamp2[k_gyro-1]);
+//        float ry=((st-timeStamp2[k_gyro-1])*rotY[k_gyro]+(timeStamp2[k_gyro]-st)*rotX[k_gyro-1])/(timeStamp2[k_gyro]-timeStamp2[k_gyro-1]);
+//        float rz=((st-timeStamp2[k_gyro-1])*rotZ[k_gyro]+(timeStamp2[k_gyro]-st)*rotX[k_gyro-1])/(timeStamp2[k_gyro]-timeStamp2[k_gyro-1]);
+//        float rx=((st-timeStamp2[k_gyro-1])*rotX[k_gyro]+(timeStamp2[k_gyro]-st)*rotX[k_gyro-1])/(timeStamp2[k_gyro]-timeStamp2[k_gyro-1]);
+//        float ry=((st-timeStamp2[k_gyro-1])*rotY[k_gyro]+(timeStamp2[k_gyro]-st)*rotX[k_gyro-1])/(timeStamp2[k_gyro]-timeStamp2[k_gyro-1]);
+//        float rz=((st-timeStamp2[k_gyro-1])*rotZ[k_gyro]+(timeStamp2[k_gyro]-st)*rotX[k_gyro-1])/(timeStamp2[k_gyro]-timeStamp2[k_gyro-1]);
+
+        
         
         float w = 960;//1920;
         float h = 544;//1080;
@@ -205,18 +247,20 @@ int main(int argc, char **argv)
 //        else if(dy>100)  dy=100;
 //        if(da<-0.5)     da=-0.5;
 //        else if(da>0.5) da=0.5;
-        
+
+/*
         T.at<double>(0,0) = cos(da);
         T.at<double>(0,1) = -sin(da);
         T.at<double>(1,0) = sin(da);
         T.at<double>(1,1) = cos(da);
         T.at<double>(0,2) = dx;
         T.at<double>(1,2) = dy;
-
         
+        warpAffine(cur, cur2, T, cur.size());
+*/
         //-------------------------------------------------------------------------------------------
         // Rodorigue
-        
+
         Mat A1(4,3,CV_64F);
         A1.at<double>(0,0) = 1; A1.at<double>(0,1) = 0; A1.at<double>(0,2) = -w/2;
         A1.at<double>(1,0) = 0; A1.at<double>(1,1) = 1; A1.at<double>(1,2) = -h/2;
@@ -233,9 +277,11 @@ int main(int argc, char **argv)
 
         Mat rotation_vector(3,1,CV_64F);
         Mat R(3,3,CV_64F);
-        rotation_vector.at<double>(0,0)=-ry;
-        rotation_vector.at<double>(1,0)=-rx;
-        rotation_vector.at<double>(2,0)=-rz;
+        
+        double gain=1.0;
+        rotation_vector.at<double>(0,0)=-ry*gain;
+        rotation_vector.at<double>(1,0)=-rx*gain;
+        rotation_vector.at<double>(2,0)=-rz*gain;
         Rodrigues(rotation_vector,R);
         
         
@@ -248,20 +294,88 @@ int main(int argc, char **argv)
         Mat T2(3,3,CV_64F);
         T2 = A2*(R*(T_*A1));
         
-        warpPerspective(cur, cur2, T2, cur.size());
+        double deno=T2.at<double>(2,2);
+        T2.at<double>(0,0) /= deno; T2.at<double>(0,1) /= deno; T2.at<double>(0,2) /= deno;//A2.at<double>(0,3) = 0;
+        T2.at<double>(1,0) /= deno; T2.at<double>(1,1) /= deno; T2.at<double>(1,2) /= deno;//A2.at<double>(1,3) = 0;
+        T2.at<double>(2,0) /= deno; T2.at<double>(2,1) /= deno; T2.at<double>(2,2) /= deno;  //A2.at<double>(2,3) = 0;
 
+//        printf("%0.3lf, %0.3lf, %0.3lf\n",T2.at<double>(0,0),T2.at<double>(0,1),T2.at<double>(0,2));
+//        printf("%0.3lf, %0.3lf, %0.3lf\n",T2.at<double>(1,0),T2.at<double>(1,1),T2.at<double>(1,2));
+//        printf("%0.3lf, %0.3lf, %0.3lf\n",T2.at<double>(2,0),T2.at<double>(2,1),T2.at<double>(2,2));
+
+        float rec=0.95;
+        Tsm.at<double>(0,0) = rec*Tsm.at<double>(0,0)+(1-rec)*T2.at<double>(0,0);
+        Tsm.at<double>(0,1) = rec*Tsm.at<double>(0,1)+(1-rec)*T2.at<double>(0,1);
+        Tsm.at<double>(0,2) = rec*Tsm.at<double>(0,2)+(1-rec)*T2.at<double>(0,2);
+        Tsm.at<double>(1,0) = rec*Tsm.at<double>(1,0)+(1-rec)*T2.at<double>(1,0);
+        Tsm.at<double>(1,1) = rec*Tsm.at<double>(1,1)+(1-rec)*T2.at<double>(1,1);
+        Tsm.at<double>(1,2) = rec*Tsm.at<double>(1,2)+(1-rec)*T2.at<double>(1,2);
+        Tsm.at<double>(2,0) = rec*Tsm.at<double>(2,0)+(1-rec)*T2.at<double>(2,0);
+        Tsm.at<double>(2,1) = rec*Tsm.at<double>(2,1)+(1-rec)*T2.at<double>(2,1);
+        Tsm.at<double>(2,2) = rec*Tsm.at<double>(2,2)+(1-rec)*T2.at<double>(2,2);
+
+        Mat T2d(3,3,CV_64F);
+        
+        T2d=T2*Tsm.inv();
+        
+        
+        if(T2d.at<double>(0,0)< 0.9)    T2d.at<double>(0,0) =   0.9;
+        else if(T2d.at<double>(0,0)>1.1)T2d.at<double>(0,0) =   1.1;
+        if(T2d.at<double>(1,1)<0.9)     T2d.at<double>(1,1) =   0.9;
+        else if(T2d.at<double>(1,1)>1.1)T2d.at<double>(1,1) =   1.1;
+        
+        if(T2d.at<double>(0,1)< -0.1)    T2d.at<double>(0,1) =   -0.1;
+        else if(T2d.at<double>(0,1)>0.1)T2d.at<double>(0,1) =   0.1;
+
+        if(T2d.at<double>(1,0)< -0.1)    T2d.at<double>(1,0) =   -0.1;
+        else if(T2d.at<double>(1,0)>0.1)T2d.at<double>(1,0) =   0.1;
+
+        float xth=100;
+        if(T2d.at<double>(0,2)< -xth)       T2d.at<double>(0,2) =   -xth;
+        else if(T2d.at<double>(0,2)>xth)    T2d.at<double>(0,2) =   xth;
+
+        float yth=60;
+        if(T2d.at<double>(1,2)< -yth)       T2d.at<double>(1,2) =   -yth;
+        else if(T2d.at<double>(1,2)>yth)    T2d.at<double>(1,2) =   yth;
+        
+        
+//        −0.1 ≤ bt, ct ≤ 0.1, −0.05 ≤ bc + ct ≤ 0.05, and
+//        −0.1 ≤ at − dt ≤ 0.1.
+        
+//        warpPerspective(cur, cur2, T2, cur.size());
+        
+        printf("%0.3lf, %0.3lf, %0.3lf\n",T2d.at<double>(0,0),T2d.at<double>(0,1),T2d.at<double>(0,2));
+        printf("%0.3lf, %0.3lf, %0.3lf\n",T2d.at<double>(1,0),T2d.at<double>(1,1),T2d.at<double>(1,2));
+        printf("%0.3lf, %0.3lf, %0.3lf\n",T2d.at<double>(2,0),T2d.at<double>(2,1),T2d.at<double>(2,2));
+
+        warpPerspective(cur, cur2, T2d, cur.size());
+
+        
+        //処理領域を設定
+        cv::Rect roi(xth, yth, cur2.cols-2*xth, cur2.rows-2*yth);
+        //cv::Rect roi(xth, yth, 100, 100);
+        //出力の初期化(入力画像を複製)
+        cv::Mat cur2_ = cur2.clone();
+        
+        //ROIの設定
+        cv::Mat cur2_roi = cur2_(roi).clone();
+        resize( cur2_roi, cur2, cur2.size(), 0, 0, INTER_LANCZOS4);
         //-------------------------------------------------------------------------------------------
         
         
-        //warpAffine(cur, cur2, T, cur.size());
+        
         
         
         //video writer
         if(k==0){
 //            writer.open("/Users/itoyuichi/github/playground/OpenCV/stab-opencv/stabilized_result.avi", CV_FOURCC_DEFAULT, 30, Size(cur.cols, cur.rows));
             canvas = Mat::zeros(cur.rows, cur.cols*2+10, cur.type());
-            writer.open("/Users/itoyuichi/github/playground/OpenCV/stab-opencv/stabilized_result4.avi", CV_FOURCC_DEFAULT, 30, Size(canvas.cols, canvas.rows));
+
             
+            
+            strcpy(path_,path);
+            writer.open(strcat(strcat(path_,fn),fndst), CV_FOURCC_DEFAULT, 30, Size(canvas.cols, canvas.rows));
+            printf("%s\n",path_);
         }
         cur.copyTo(canvas(Range::all(), Range(0, cur2.cols)));
         cur2.copyTo(canvas(Range::all(), Range(cur2.cols+10, cur2.cols*2+10)));
